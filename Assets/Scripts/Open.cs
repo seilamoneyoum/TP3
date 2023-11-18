3997109b2805ce3bb22345bd1418e77a0540bc2b
Seilamoney Oum <seilamoney2002@gmail.com>
2023-11-18 15:33:54 -0500

    Movement

--- Assets/Scripts/Open.cs
+++ Assets/Scripts/Open.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveDoor : MonoBehaviour
+public class Open : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,17 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 5f;
+    [SerializeField] private Transform mainCamera;
+    private CharacterController characterController;
+    private Vector3 direction;
+    private float rotationTime = 0.1f;
+    private float rotationSpeed;
+    private float gravity = 20f;
+    private float verticalMovement = 0f;
+
+    float horizontal, vertical, targetAngle, angle, tempSpeed, originalMovementMagnitude;
+
+    private void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        BuildSurfaceMovement();
+        BuildVerticalMovement();
+        characterController.Move(direction);
+    }
+
+    private void BuildSurfaceMovement()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        direction = new Vector3(horizontal, 0f, vertical);
+
+        if (direction.magnitude > 1.0f) direction = direction.normalized;
+
+
+        if (direction.magnitude >= 0.1f)
+        {
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
+
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSpeed, rotationTime);
+
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            tempSpeed = moveSpeed;
+            if (!characterController.isGrounded) tempSpeed /= 2;
+
+            Vector3 directionWithCamera = (Quaternion.Euler(0f, angle, 0f) * Vector3.forward).normalized;
+            originalMovementMagnitude = direction.magnitude;
+            direction.x = directionWithCamera.x * tempSpeed * originalMovementMagnitude * Time.deltaTime;
+            direction.z = directionWithCamera.z * tempSpeed * originalMovementMagnitude * Time.deltaTime;
+        }
+        else
+        {
+            direction = Vector3.zero;
+        }
+    }
+    private void BuildVerticalMovement()
+    {
+        if (!characterController.isGrounded) verticalMovement -= gravity * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && characterController.isGrounded)
+        {
+            verticalMovement = jumpSpeed;
+        }
+
+        direction.y = verticalMovement * Time.deltaTime;
     }
 }
