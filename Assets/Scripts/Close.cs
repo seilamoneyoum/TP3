98abfb6b316db6339e1ac62ae2605cb058646334
Seilamoney Oum <seilamoney2002@gmail.com>
2023-11-19 16:38:02 -0500

    Open/Close fait

--- Assets/Scripts/Close.cs
+++ Assets/Scripts/Close.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Close : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject objectToOpen;
+    private void Awake()
     {
-        
+        if (transform.Find("DoorSingle") != null)
+        {
+            Transform childTransform = transform.Find("DoorSingle");
+            objectToOpen = childTransform.gameObject;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.GetComponent<CharacterController>() != null)
+        {
+            Vector3 newRotation = new Vector3(objectToOpen.transform.rotation.x, 0, objectToOpen.transform.rotation.z);
+            objectToOpen.transform.rotation = Quaternion.Euler(newRotation);
+        }
     }
 }
--- Assets/Scripts/Open.cs
+++ Assets/Scripts/Open.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Open : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject objectToOpen;
+    private void Awake()
     {
-        
+        if (transform.Find("DoorSingle") != null)
+        {
+            Transform childTransform = transform.Find("DoorSingle");
+            objectToOpen = childTransform.gameObject;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay(Collider other)
     {
-        
+        if (other.GetComponent<CharacterController>() != null)
+        {
+            Vector3 newRotation = new Vector3(objectToOpen.transform.rotation.eulerAngles.x, 90f, objectToOpen.transform.rotation.eulerAngles.z);
+            objectToOpen.transform.rotation = Quaternion.Euler(newRotation);
+        }
     }
 }
