4b6a113777806d2f01b3769da37656119c56531b
Seilamoney Oum <seilamoney2002@gmail.com>
2023-11-19 22:18:23 -0500

    GameManager ajusté

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.XR;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
+    private const int NB_TOTAL_KEYS = 5;
+    private const int NB_TOTAL_CLUES = 10;
     private const int SECONDS_IN_ONE_MIN = 60;
     private const int POINTS_PER_REMAINING_SECONDS = 100;
     private const int INDEX_FOR_TITLE = 0;
@@ -13,15 +18,14 @@ public class GameManager : MonoBehaviour
     private static GameManager instance = null;
     public static GameManager Instance { get { return instance; } }
     private int actualScene = 0;
-    private int score = 0;
     bool scenesAreInTransition = false;
     private bool gameStart = false;
     private float remainingTime = 480;
-    Text scoreText;
+    List<GameObject> clues = new List<GameObject>();
+    List<GameObject> keys = new List<GameObject>();
     Text nbCluesText;
     Text nbKeysText;
     Text timeText;
-
     void Awake()
     {
         if (instance == null)
@@ -41,22 +45,45 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         if (Input.GetButtonDown("Cancel")) Application.Quit();
-        if (Input.GetButtonDown("Submit")) LoadScene(1, INDEX_FOR_MAIN);
         if (gameStart == true)
         {
-            float minutes = Mathf.Floor(remainingTime / SECONDS_IN_ONE_MIN);
-            float seconds = remainingTime % SECONDS_IN_ONE_MIN;
-            timeText.text = minutes + " : " + seconds;
-            remainingTime -= Time.deltaTime;
-            if (remainingTime <= 0)
-            {
-                gameStart = false;
-                LoadScene(20, INDEX_FOR_END);
-            }
-
+            UpdateTime();
+        }
+        else
+        {
+            if (Input.GetButtonDown("Submit")) LoadScene(0, INDEX_FOR_MAIN);
 
         }
     }
+
+    public void AddKey(GameObject gameObject)
+    {
+        keys.Add(gameObject);
+        nbKeysText = GameObject.Find("NbKeysText").GetComponent<Text>();
+        nbKeysText.text = keys.Count.ToString() + "/" + NB_TOTAL_KEYS;
+    }
+
+    public void AddHint(GameObject gameObject)
+    {
+        clues.Add(gameObject);
+        nbCluesText = GameObject.Find("NbCluesText").GetComponent<Text>();
+        nbCluesText.text = keys.Count.ToString() + "/" + NB_TOTAL_CLUES;
+    }
+
+    private void UpdateTime()
+    {
+        timeText = GameObject.Find("TimeText").GetComponent<Text>();
+        float minutes = Mathf.Floor(remainingTime / SECONDS_IN_ONE_MIN);
+        float seconds = remainingTime % SECONDS_IN_ONE_MIN;
+        timeText.text = (int)minutes + " : " + (int)seconds;
+        remainingTime -= Time.deltaTime;
+        if (remainingTime <= 0)
+        {
+            gameStart = false;
+            LoadScene(20, INDEX_FOR_END);
+        }
+    }
+
 
     public void LoadScene(float delay, int scene)
     {
@@ -74,12 +101,16 @@ public class GameManager : MonoBehaviour
         switch(scene)
         {
             case 0:
+                actualScene = INDEX_FOR_TITLE;
                 SceneManager.LoadScene("Title");
                 break;
             case 1:
+                gameStart = true;
+                actualScene = INDEX_FOR_MAIN;
                 SceneManager.LoadScene("Main");
                 break;
             case 2:
+                actualScene = INDEX_FOR_END;
                 SceneManager.LoadScene("End");
                 break;
         }
@@ -87,10 +118,6 @@ public class GameManager : MonoBehaviour
         scenesAreInTransition = false;
     }
 
-    public void AddScore(int scoreToAdd)
-    {
-        score += scoreToAdd;
-        scoreText.text = score.ToString();
-    }
+
 
 }
--- Assets/Scripts/Open.cs
+++ Assets/Scripts/Open.cs
@@ -5,26 +5,23 @@ using UnityEngine;
 public class Open : MonoBehaviour
 {
     GameObject moveableObject;
+    AudioSource audioSource;
     private void Start()
     {
+        audioSource = this.GetComponent<AudioSource>();
         MoveableObject moveableGameObject = GetComponent<MoveableObject>();
         moveableObject = moveableGameObject.GetMoveableObject();
     }
-    private void Awake()
-    {
-        if (transform.Find("DoorSingle") != null)
-        {
-            Transform childTransform = transform.Find("DoorSingle");
-            moveableObject = childTransform.gameObject;
-        }
-    }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<CharacterController>() != null)
         {
+            audioSource.Play();
             Vector3 newRotation = new Vector3(moveableObject.transform.rotation.eulerAngles.x, 90f, moveableObject.transform.rotation.eulerAngles.z);
             moveableObject.transform.rotation = Quaternion.Euler(newRotation);
         }
-    }
+    
+}
+
 }
